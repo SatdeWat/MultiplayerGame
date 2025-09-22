03d039a235e2a41641d727c6f1b51dbe3ac23b88
SatdeWat <sethdw0610@gmail.com>
2025-09-22 12:12:46 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,20 @@
 // firebase-config.js
+// Dit bestand initialiseert Firebase en exporteert 'db' voor gebruik in andere modules.
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
 
+/* VERVANG DIT MET JE EIGEN CONFIG ALS JE WIL */
 const firebaseConfig = {
   apiKey: "AIzaSyCdI588pB7GMPJcjDJTHLAWjOmADixFnvw",
   authDomain: "zeeslag-game.firebaseapp.com",
   databaseURL: "https://zeeslag-game-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "zeeslag-game",
-  storageBucket: "zeeslag-game.firebasestorage.app",
+  storageBucket: "zeeslag-game.firebasedomain.app",
   messagingSenderId: "638867155979",
   appId: "1:638867155979:web:58b7c53a5c2bf734782b0e"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export default firebaseConfig;
