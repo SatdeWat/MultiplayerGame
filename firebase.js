3368f275c2f0c3741977c93737f271368946410d
SatdeWat <sethdw0610@gmail.com>
2025-11-06 10:50:42 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,7 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import {
-  getDatabase,
-  ref as dbRef,
-  get as dbGet,
-  set as dbSet,
-  onValue as dbOnValue,
-  push as dbPush,
-  update as dbUpdate,
-  remove as dbRemove
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getDatabase, ref, set, get, update, onValue, push, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-// --- jouw Firebase config (zoals door jou gegeven) ---
 const firebaseConfig = {
   apiKey: "AIzaSyCdI588pB7GMPJcjDJTHLAWjOmADixFnvw",
   authDomain: "zeeslag-game.firebaseapp.com",
@@ -22,7 +12,6 @@ const firebaseConfig = {
   appId: "1:638867155979:web:58b7c53a5c2bf734782b0e"
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, dbRef, dbGet, dbSet, dbOnValue, dbPush, dbUpdate, dbRemove };
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export { ref, set, get, update, onValue, push, remove };
