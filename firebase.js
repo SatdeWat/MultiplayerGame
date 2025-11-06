581aef75cafb55a035ca6da4c087965b6621d736
SatdeWat <sethdw0610@gmail.com>
2025-11-06 12:41:34 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getDatabase, ref, set, get, update, onValue, push, remove } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getDatabase, ref, set, update, get, onValue } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdI588pB7GMPJcjDJTHLAWjOmADixFnvw",
   authDomain: "zeeslag-game.firebaseapp.com",
   databaseURL: "https://zeeslag-game-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "zeeslag-game",
-  storageBucket: "zeeslag-game.firebasestorage.app",
+  storageBucket: "zeeslag-game.appspot.com",
   messagingSenderId: "638867155979",
   appId: "1:638867155979:web:58b7c53a5c2bf734782b0e"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export { ref, set, get, update, onValue, push, remove };
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, set, update, get, onValue };
